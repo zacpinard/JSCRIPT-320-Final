c9d8b232721a99f5bb491e95fb0c3a108cbc3ac0
zacpinard <zacpinard@gmail.com>
2026-02-26 21:26:39 -0800

    add hosting

--- Washington-Climate-Commitment/src/App.jsx
+++ Washington-Climate-Commitment/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    "Washington's Climate Commitment Visualized"
+    "Washington's Climate Commitment Visualized - this is the development branch"
   )
 }
 
