2b27f1278aba2c448cdff8d9328d2d6e1b695a59
zacpinard <zacpinard@gmail.com>
2026-03-07 15:04:02 -0800

    Getting polluter pages and map on polluter pages to work

--- Washington-Climate-Commitment/src/PolluterPage.jsx
+++ Washington-Climate-Commitment/src/PolluterPage.jsx
@@ -35,22 +35,24 @@ export default function PolluterPage() {
 
   }, [id])
 
+  if (!polluter) return <div>Loading...</div>
 
   return (
     <div>
-      <h1>Polluter ID: {id}</h1>
-      <h2>Name: {polluter.name}</h2>
-      <MapContainer center={[polluter.latitude, polluter.longitude]} zoom={7} scrollWheelZoom={false} className="leaflet-container">
-            <TileLayer 
-              url='https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}'
-              attribution="Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ"
-            />
-            <Marker position={[polluter.latitude, polluter.longitude]}>
-              <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-              </Popup>
-            </Marker>
-          </MapContainer>
+      <h1>Polluter Name: {polluter.name}</h1>
+      {polluter.latitude && polluter.longitude && (
+        <MapContainer center={[polluter.latitude, polluter.longitude]} zoom={7} scrollWheelZoom={false} className="leaflet-container">
+          <TileLayer
+            url='https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}'
+            attribution="Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ"
+          />
+          <Marker position={[polluter.latitude, polluter.longitude]}>
+            <Popup>
+              {polluter.name} location
+            </Popup>
+          </Marker>
+        </MapContainer>
+      )}
       <h2>Industry: {polluter.industry}</h2>
       <h2>Type of Market Participant: {polluter.status}</h2>
       <h2>Location: {polluter.location}</h2>
