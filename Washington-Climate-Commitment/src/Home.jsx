1ed83bf5f65b51a6507935c8db303863a0ee5748
zacpinard <zacpinard@gmail.com>
2026-03-08 16:47:15 -0700

    CCA explanation

--- Washington-Climate-Commitment/src/Home.jsx
+++ Washington-Climate-Commitment/src/Home.jsx
@@ -12,6 +12,9 @@ export function Home() {
       <div className='whole-thing'>
         <YearContainer />
         <div className='polluter-map-container'>
+          <div className='text-explanation-container'>
+            In Washington, contributnig to climate change is not free. Our Climate Commitment Act takes all major carbon polluters (greater than 25,000 metric tons per year) and places them into two categories. Regular COVERED polluters have to pay one 'allowance' per metric ton of carbon equivalent they pollute. Other EMISSTIONS INTENSIVE, TRADE-EXPOSED, or EITE polluters are given a specified amount of allowances for free, based on their previous emissions. A polluter that doesn't decrease their carbon pollution must pay for more allowances. Both pools of allowances decrease over time, so regardless of which polluters buy more or fewer allowances, the total number of allowances -and therefore the total number of emissions in Washington state- decreases. Here we visualize Washington's pool of allowances over time on the left, and major sources of carbon pollution on the right.
+          </div>
           <PolluterProfiles />
           <CreateMap />
         </div>
