1c4753b7401ba0ef39cfba8ec024c449db2da157
zacpinard <zacpinard@gmail.com>
2026-03-08 21:44:27 -0700

    unit testing

--- Washington-Climate-Commitment/src/YearEmissionsTotal.test.js
+++ Washington-Climate-Commitment/src/YearEmissionsTotal.test.js
@@ -4,14 +4,6 @@ const getTotalEmissions = (allowances_sold, eite_allocation) => {
 
 describe('getTotalEmissions', () => {
   test('adds two numbers correctly', () => {
-    expect(getTotalEmissions(1000000, 500000)).toBe(1500000);
-  });
-
-  test('handles zero values', () => {
-    expect(getTotalEmissions(0, 0)).toBe(0);
-  });
-
-  test('handles one zero', () => {
-    expect(getTotalEmissions(1000000, 0)).toBe(1000000);
+    expect(getTotalEmissions(14, 15)).toBe(29);
   });
 });
