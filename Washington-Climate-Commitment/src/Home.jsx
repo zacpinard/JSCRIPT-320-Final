59872fb69af4dceca395e47504f29fd012ed6141
zacpinard <zacpinard@gmail.com>
2026-03-08 20:21:13 -0700

    fixing typo

--- Washington-Climate-Commitment/src/Home.jsx
+++ Washington-Climate-Commitment/src/Home.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import CreateMap from './map'
-import PolluterProfiles from './PolluterProfiles'
+import PolluterProfiles from './Polluter_Profiles'
 import { Link } from 'react-router-dom'
 import YearContainer from './YearContainer'
 
