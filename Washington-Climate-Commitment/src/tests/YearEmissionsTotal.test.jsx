7399717b4bd14a28cdc89ae49438e3ac425511e6
zacpinard <zacpinard@gmail.com>
2026-03-09 22:29:29 -0700

    editing tests

--- Washington-Climate-Commitment/src/tests/YearEmissionsTotal.test.jsx
+++ Washington-Climate-Commitment/src/tests/YearEmissionsTotal.test.jsx
@@ -1,9 +1,13 @@
+import { describe, it } from "vitest";
+
+
+
 const getTotalEmissions = (allowances_sold, eite_allocation) => {
   return allowances_sold + eite_allocation;
 };
 
 describe('getTotalEmissions', () => {
-  test('adds two numbers correctly', () => {
+  it('adds two numbers correctly', () => {
     expect(getTotalEmissions(14000000, 15000000)).toBe(29000000);
   });
 });
--- Washington-Climate-Commitment/vite.config.js
+++ Washington-Climate-Commitment/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['src/tests/setup.js'],
+    testMatch: ['src/tests/**/*.test.jsx'],
+    globals: true,
+  }
 })
